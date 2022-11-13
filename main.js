0e00f26d238cb4b9fabaaf883fe595d4986ff8a2
zankokun <dinarik953@gmail.com>
2022-11-14 01:08:48 +0300

    fixed progress bars

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function setBaseText()
     words1s = slider.value / 60;
 }
 
-function showErr(valueHtml) 
+function showErr(valueHtml)
 {
     var text = document.createElement('h1');
     var errArea = document.createElement('div');
@@ -76,15 +76,80 @@ function Play()
     const displayTime = sentences.map(element => {
         let wordsInSentence = element.split(' ');
         wordsInSentence.forEach(word => {
-            word = word.length; 
+            word = word.length;
         });
 
-        const average = wordsInSentence.reduce((a, b) => a + b.length, -1) / wordsInSentence.length;
-        element = element.length / (average * words1s);
-        return element;
+        const average = wordsInSentence.reduce((a, b) => a + b.length, 0) / wordsInSentence.length;
+        let time = element.length / (average * words1s);
+        return time;
     });
 
+
+    var maxWidth = canvas[0].width * 0.96;
+    var index = 0;
+    var intervals = []
+    var timeouts = []
+    var lastIsCleared = false;
+
+    function func() {
+        let internalIndex = index;
+        let index3  = internalIndex % 3;
+        if(internalIndex == sentences.length || isPlaying == false){
+            lastIsCleared = true;
+            ctx.forEach((element, i) => {
+                element.clearRect(0, 0, canvas[i].width, canvas[i].height);
+            });
+            intervals.forEach((id)=>{
+                clearInterval(id);
+            });
+            timeouts.forEach((id)=>{
+                clearTimeout(id);
+            });
+            isPlaying = false;
+            return;
+        }
+        else if(internalIndex % 3 == 0 && internalIndex > 0){
+            lastIsCleared = true;
+            ctx.forEach((element, i) => {
+                element.clearRect(0, 0, canvas[i].width, canvas[i].height);
+            });
+        }
+        ctx[index3].font = "22px Tahoma";
+        ctx[index3].fillStyle = "black";
+        ctx[index3].clearRect(0, 0, canvas[index3].width, canvas[index3].height);
+        ctx[index3].fillText(sentences[internalIndex], canvas[index3].width * 0.02, canvas[index3].height / 2, maxWidth);
+
+        let progressBar = 0;
+        let progress = setInterval(() => {
+            if (progressBar == 100 || isPlaying == false) {
+                clearInterval(progress);
+                if(lastIsCleared == false) {
+                    ctx[index3].clearRect(0, 0, canvas[index3].width, canvas[index3].height);
+                    ctx[index3].fillStyle = "darkgrey";
+                    ctx[index3].font = "20px Tahoma";
+                    ctx[index3].fillText(sentences[internalIndex], canvas[index3].width * 0.02, canvas[index3].height / 2, maxWidth);
+                }
+                else{
+                    lastIsCleared = false;
+                }
+                return;
+            }
+            ctx[index3].fillRect(canvas[index3].width * 0.02, canvas[index3].height * 0.98, ctx[index3].measureText(sentences[internalIndex]).width  * progressBar / 100, canvas[index3].height * 0.02);
+            progressBar++;
+        }, displayTime[internalIndex] * 10);
+        intervals.push(progress);
+        let id = setTimeout(func, displayTime[internalIndex]*1000);
+        timeouts.push(id);
+        index++;
+    };
+
     document.getElementById("stop").onclick = () => {
+        intervals.forEach((id)=>{
+            clearInterval(id);
+        });
+        timeouts.forEach((id)=>{
+            clearTimeout(id);
+        });
         ctx.forEach((element, i) => {
             isPlaying = false;
             element.font = "20px Tahoma";
@@ -94,75 +159,7 @@ function Play()
         ctx[0].font = "22px Tahoma";
     };
 
-    var maxWidth = canvas[0].width * 0.96;
-    ctx[0].fillStyle = "black";
-    ctx.forEach((element, index) => {
-        if (sentences[index] != undefined) element.fillText(sentences[index], canvas[0].width * 0.02, canvas[0].height / 2, maxWidth);
-    });
-
-    let progressBar = 0;
-    let progress = setInterval(() => {
-        if (progressBar == 100) clearInterval(progress);
-        ctx[0].fillRect(0, canvas[0].height * 0.98, ctx[0].measureText(sentences[0]).width / 100 * progressBar, canvas[0].height * 0.02);
-        progressBar++;
-    }, displayTime[0] / 100 * 1000);
-    delete progressBar;
-    delete progress;
-    console.log(sentences);
-
-    var index = 1;
-    var timer = setTimeout(function func() {
-        if (isPlaying == false) {
-            clearInterval(progress);
-            return;
-        }
-
-        if (index != 0) {
-            if (index % 3 == 0) {
-                ctx.forEach((element, i) => {
-                    element.font = "20px Tahoma";
-                    element.fillStyle = "darkgrey";
-                    element.clearRect(0, 0, canvas[i].width, canvas[i].height);
-                    if (sentences[index + i] != undefined) element.fillText(sentences[index + i], canvas[0].width * 0.02, canvas[0].height / 2, maxWidth);
-                });
-            }
-
-            ctx[index % 3].font = "22px Tahoma";
-            ctx[index % 3].fillStyle = "black";
-            ctx[index % 3].clearRect(0, 0, canvas[0].width, canvas[0].height);
-            if (sentences[index] != undefined) ctx[index % 3].fillText(sentences[index], canvas[0].width * 0.02, canvas[0].height / 2, maxWidth);
-
-            if (index >= 1 && index % 3 != 0) {
-                ctx[(index - 1) % 3].clearRect(0, 0, canvas[0].width, canvas[0].height);
-                ctx[(index - 1) % 3].fillStyle = "darkgrey";
-                ctx[(index - 1) % 3].font = "20px Tahoma";
-                ctx[(index - 1) % 3].fillText(sentences[index - 1], canvas[0].width * 0.02, canvas[0].height / 2, maxWidth);
-                ctx[(index - 1) % 3].clearRect(0, canvas[0].height * 0.98, canvas[0].width, canvas[0].height);
-            }
-
-            let progressBar = 0;
-            let progress = setInterval(() => {
-                console.log(ctx[(index -1) % 3].measureText(sentences[index]));
-                if (progressBar == 100) {clearInterval(progress); delete progressBar;}
-                ctx[(index - 1) % 3].fillRect(0, canvas[0].height * 0.98, ctx[0].measureText(sentences[index]).width / 100 * progressBar, canvas[0].height * 0.02);
-                progressBar++;
-            }, displayTime[index] / 100 * 1000);
-
-            if (sentences.length == index) {
-                ctx.forEach(element => {
-                    element.font = "20px Tahoma";
-                    element.fillStyle = "darkgrey";
-                    element.clearRect(0, 0, canvas[0].width, canvas[0].height);
-                    ctx[0].font == "22px Tahoma";
-                });
-                isPlaying = false;
-                return;
-            };
-        }
-
-        setTimeout(func, displayTime[index] * 1000);
-        index++;
-    }, displayTime[index] * 1000);
+    func();
 }
 
 //set base parameters on load
@@ -170,7 +167,7 @@ window.onload = function() {
     var canvas = Array.from(document.getElementsByClassName("canvas"));
     canvas.forEach(element => {
         element.width = element.parentElement.offsetWidth;
-        element.height = element.parentElement.offsetHeight / 3; 
+        element.height = element.parentElement.offsetHeight / 3;
 
         let ctx = element.getContext("2d");
         ctx.font = "20px Tahoma";
@@ -191,9 +188,8 @@ window.addEventListener("resize", function() {
     var canvas = Array.from(document.getElementsByClassName("canvas"));
     canvas.forEach(element => {
         element.width = element.parentElement.offsetWidth;
-        element.height = element.parentElement.offsetHeight / 3; 
+        element.height = element.parentElement.offsetHeight / 3;
     });
 });
 
 //:) it's not so easy
-
